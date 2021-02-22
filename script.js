ec91c87c539c86cfccb9df1893f8881ffcc19b49
Caique <caiquesouzaresponsa@hotmail.com>
2021-02-22 11:53:00 -0300

    Add script.jsestlio.csscontadordenumerosindex.htm

---
+++ script.js
@@ -0,0 +1,34 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if  (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('verifique os dados')
+        res.innerHTML = 'impossivel contar'
+    } else {
+        res.innerHTML = 'contando:<br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('passo inválido considerando passo 1')
+            p = 1
+        }
+        if (i < f ) {
+            //contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c}\u{1F449}`
+        }
+    } else {
+        // contagem regressiva
+        for(let c = i; c >= f; c -= p){
+            res.innerHTML +=`${c}\u{1f449}`
+
+        }
+     }
+    res.innerHTML += `\u{1f3c1}`
+
+    }
+}
